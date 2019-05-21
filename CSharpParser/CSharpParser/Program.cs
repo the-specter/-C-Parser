bd17dcd10b02971c45908aca6f7bb39dd32f248c
Roman S <38585555+era312@users.noreply.github.com>
2019-05-22 02:22:18 +0300

    delete class constant & add Selenium

--- CSharpParser/CSharpParser/Program.cs
+++ CSharpParser/CSharpParser/Program.cs
@@ -10,24 +10,30 @@ namespace CSharpParser
 {
     class Program
     {
-
         static void Main(string[] args)
         {
-            Console.WriteLine("Loading . . .");
+            Console.WriteLine("Загрузка . . .");
+
+            Uri Anidub = new Uri("https://anime.anidub.com/");
+            string ChromeUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
+            string UTF8 = "utf-8";
 
             WebClient Client = new WebClient();
-            Client.Encoding = Encoding.GetEncoding(Constants.UTF8); // Изменение стандартной кодировки на uft - 8 для отображения русских символов
-            Client.Headers.Add("user-agent", Constants.ChromeUserAgent); // User-Agent
+            Client.Encoding = Encoding.GetEncoding(UTF8); // Изменение стандартной кодировки на uft - 8 для отображения русских символов
+            Client.Headers.Add("user-agent", ChromeUserAgent); // User-Agent
 
-            Console.WriteLine("Start download site . . .");
-            Stream Site = Client.OpenRead(Constants.Anidub); // Загружает сайт
-            StreamReader SiteReader = new StreamReader(Site, Encoding.GetEncoding(Constants.UTF8)); // Считывает сайт
-            Console.WriteLine("Download done . . .");
+            Console.WriteLine("Загрузка сайта . . .");
+            Stream Site = Client.OpenRead(Anidub); // Загружает сайт
+            StreamReader SiteReader = new StreamReader(Site, Encoding.GetEncoding(UTF8)); // Считывает сайт
+            Console.WriteLine("Сайт загружен . . .");
 
             Console.WriteLine(SiteReader.ReadToEnd()); // Выводит код страницы
-            
 
-            Console.ReadKey();
+            // Закрытие потоков
+            Site.Close();
+            SiteReader.Close();
+
+            Console.ReadKey(); // Ожидание нажатия клавиши
         }
     }
 }
