e732104489342d808cabb9947aa3e84f6c69c461
Roman S <38585555+era312@users.noreply.github.com>
2019-05-22 01:32:33 +0300

    Add WebClient & Add user-agent & Create Constant class

---
+++ CSharpParser/CSharpParser/Constants.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpParser
+{
+    class Constants
+    {
+
+        public static string ChromeUserAgent
+        {
+            get
+            {
+                return "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36";
+            }
+            set { }
+        }
+        
+    }
+}
--- CSharpParser/CSharpParser/Program.cs
+++ CSharpParser/CSharpParser/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System;
-using System.Web; // Для работы с Интернетом
+using System.Net; // Для работы с Интернетом
 using System.Threading; // Для многопоточности
+using System.Diagnostics; // Для взаимодействия с внешними процесами
+using CSharpParser;
 
 
 namespace CSharpParser
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            WebClient Client = new WebClient();
+            Client.Headers.Add("user-agent", Constants.ChromeUserAgent);
             Console.WriteLine("Hello World!");
-            
         }
     }
 }
