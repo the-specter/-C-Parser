9dd8cc007ad49a1f44771c363838e3419def6bef
Roman S <38585555+era312@users.noreply.github.com>
2019-05-22 01:13:00 +0300

    Hello GitHub

---
+++ CSharpParser/CSharpParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web; // Для работы с Интернетом
+using System.Threading; // Для многопоточности
+
+
+namespace CSharpParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
